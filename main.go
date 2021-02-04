3c463865fe3d9af8120ce511a120a34756ac1cea
c45y <git@c45y.com>
2021-02-04 22:00:13 +1000

    wrap in webserver

--- main.go
+++ main.go
@@ -1,38 +1,76 @@
 package main
 
 import (
+	"image"
+	"image/png"
+	"io"
 	"log"
+	"net/http"
+	"bytes"
 
 	"github.com/fogleman/gg"
 	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font"
 	"golang.org/x/image/font/gofont/goregular"
+
+	_ "embed"
 )
 
-func main() {
-	const (
-		W = 733
-		H = 906
-	)
+//go:embed "template.png"
+var rawImage []byte
 
+type App struct {
+	ff font.Face
+	bg image.Image
+}
+
+func NewApp() (ret App) {
 	// Font
 	font, err := truetype.Parse(goregular.TTF)
 	if err != nil {
 		log.Fatal(err)
 	}
-	face := truetype.NewFace(font, &truetype.Options{Size: 48})
+	ret.ff = truetype.NewFace(font, &truetype.Options{Size: 48})
 
 	// Background
-	im, err := gg.LoadPNG("template.png")
+	ret.bg, err = png.Decode(bytes.NewReader(rawImage))
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	return
+}
+
+func (app *App) render(w io.Writer, lineOne string, lineTwo string) {
+	const (
+		W = 733
+		H = 906
+	)
+
 	dc := gg.NewContext(W, H)
-	dc.SetFontFace(face)
+	dc.SetFontFace(app.ff)
 	dc.SetRGB(0, 0, 0)
 
-	dc.DrawImage(im, 0, 0)
-	dc.DrawStringWrapped("Hello, world! and other stuff", 558, 114, 0.5, 0.5, 300, 1.1, gg.AlignCenter)
-	dc.DrawStringWrapped("Meow?", 571, 403, 0.5, 0.5, 300, 1.1, gg.AlignCenter)
-	dc.SavePNG("out.png")
+	dc.DrawImage(app.bg, 0, 0)
+	dc.DrawStringWrapped(lineOne, 558, 114, 0.5, 0.5, 300, 1.1, gg.AlignCenter)
+	dc.DrawStringWrapped(lineTwo, 571, 403, 0.5, 0.5, 300, 1.1, gg.AlignCenter)
+	if err := dc.EncodePNG(w); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (app *App) index(w http.ResponseWriter, req *http.Request) {
+	log.Println("%s %s from %s", req.Method, req.RequestURI, req.RemoteAddr)
+	w.Header().Set("Content-Type", "image/png")
+
+	lone := req.FormValue("one")
+	ltwo := req.FormValue("two")
+
+	app.render(w, lone, ltwo)
+}
+
+func main() {
+	app := NewApp()
+	http.HandleFunc("/", app.index)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
