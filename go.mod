a5ca02aad9ae3742c7a6cb94730b053445abd61a
c45y <git@c45y.com>
2021-05-25 18:43:00 +1000

    graphite integration

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/fogleman/gg v1.3.0
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	github.com/marpaia/graphite-golang v0.0.0-20190519024811-caf161d2c2b1
 	golang.org/x/image v0.0.0-20201208152932-35266b937fa6
 )
--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"image"
 	"image/png"
 	"io"
 	"log"
+	"net"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/fogleman/gg"
 	"github.com/golang/freetype/truetype"
+	"github.com/marpaia/graphite-golang"
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/gofont/goregular"
 
@@ -25,6 +30,7 @@ var rawIndex []byte
 type App struct {
 	ff font.Face
 	bg image.Image
+	g  *graphite.Graphite
 }
 
 func NewApp() (ret App) {
@@ -64,22 +70,47 @@ func (app *App) render(w io.Writer, lineOne string, lineTwo string) {
 
 func (app *App) index(w http.ResponseWriter, req *http.Request) {
 	log.Printf("%s %s from %s\n", req.Method, req.RequestURI, req.RemoteAddr)
-
-
+	start := time.Now()
 	lone := req.FormValue("l1")
 	ltwo := req.FormValue("l2")
 
 	if lone == "" && ltwo == "" {
 		w.Header().Set("Content-Type", "text/html")
 		w.Write(rawIndex)
+		app.g.SimpleSend("meme.index", strconv.FormatInt(time.Now().Sub(start).Milliseconds(), 10))
 	} else {
 		w.Header().Set("Content-Type", "image/png")
 		app.render(w, lone, ltwo)
+		app.g.SimpleSend("meme.render", strconv.FormatInt(time.Now().Sub(start).Milliseconds(), 10))
 	}
 }
 
 func main() {
+	cliGraphite := flag.String("graphite", "", "Remote Graphite host:port")
+	flag.Parse()
+
 	app := NewApp()
+
+	if *cliGraphite == "" {
+		app.g = graphite.NewGraphiteNop("", 0)
+	} else {
+		// host:prot splitter
+		host, sport, err := net.SplitHostPort(*cliGraphite)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// number conv
+		port, err := strconv.Atoi(sport)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Set prefix
+		app.g, err = graphite.NewGraphiteWithMetricPrefix(host, port, "maas")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 	http.HandleFunc("/", app.index)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
